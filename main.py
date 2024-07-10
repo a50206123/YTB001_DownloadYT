e64cf0a3e2c12572bbaf460479af596ad6275bc7
YCLin mbp <a50206123@gmail.com>
2024-07-11 00:45:06 +0800

    updated

--- main.py
+++ main.py
@@ -1,41 +1,89 @@
-import download_yt
-import tkinter as tk
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton, \
+    QFileDialog, QToolButton, QRadioButton, QTabWidget
+import sys
 
-height_widget = 1
+from download_yt import download_yt, download_yts, download_yt_playlist
 
-class DownloadYouTube(tk.Tk) :
+class main(QMainWindow) :
     def __init__(self) -> None:
         super().__init__()
 
         self.initUI()
 
     def initUI(self) :
-        self.title('Download YouTube APP')
-        self.geometry('600x100')
+        self.ui = uic.loadUi('ui/MainWindow.ui', self)
         
-        self.frame_input = tk.Frame(self, width= 550, padx= 10, pady=10)
-        self.frame_input.pack()
-
-        self.label_input = tk.Label(self.frame_input, text= 'Input YouTube Url :', height= height_widget, padx= 3, pady=3)
-        self.url_input = tk.Entry(self.frame_input)
-        self.label_folder = tk.Label(self.frame_input, text= 'Download Folder :', height= height_widget, padx= 3, pady=3)
-        self.folder_input = tk.Entry(self.frame_input)
-
-        self.label_input.grid(column=0, row=0)
-        self.url_input.grid(column=1, row=0)
-        self.label_folder.grid(column=0, row=1)
-        self.folder_input.grid(column=1, row=1)
-
-        self.frame_button = tk.Frame(self, width=550, padx=10, pady=10)
-        self.frame_button.pack()
-
-        self.btn_download = tk.Button(self.frame_button, padx=3, pady=3, text='Click to Download!', 
-                                      command= lambda (download_yt.download_yt(url=self.url_input.get(), 
-                                                                       folder=self.folder_input.get())))
+        # Def. Tab
+        self.tabWidget = self.ui.findChild(QTabWidget, 'tabWidget')
         
-        self.btn_download.grid(column=5, row=0)
+        # Def. LineEdit
+        self.le_link1 = self.ui.findChild(QLineEdit, 'le_link1')
+        self.le_link2 = self.ui.findChild(QLineEdit, 'le_link2')
+        self.le_link3 = self.ui.findChild(QLineEdit, 'le_link3')
+        self.le_link4 = self.ui.findChild(QLineEdit, 'le_link4')
+        self.le_link5 = self.ui.findChild(QLineEdit, 'le_link5')
+        self.le_links = [self.le_link1, self.le_link2, self.le_link3, 
+                         self.le_link4, self.le_link5]
+        
+        self.le_link11 = self.ui.findChild(QLineEdit, 'le_link11')
+        
+        self.le_download_path = self.ui.findChild(QLineEdit, 'le_download_path')
+        
+        # Def. Buttons
+        self.btn_select_path = self.ui.findChild(QToolButton, 'btn_select_path')
+        self.pbtn_download = self.ui.findChild(QPushButton, 'pbtn_download')
+        
+        # Def. RadioButton
+        self.rb_mp3 = self.ui.findChild(QRadioButton, 'rb_mp3')
+        self.rb_mp4 = self.ui.findChild(QRadioButton, 'rb_mp4')
+        
+        self.rb_high = self.ui.findChild(QRadioButton, 'rb_high')
+        self.rb_1080 = self.ui.findChild(QRadioButton, 'rb_1080')
+        self.rb_720 = self.ui.findChild(QRadioButton, 'rb_720')
+        
+        # Connect
+        self.pbtn_download.clicked.connect(self.download)
+        self.btn_select_path.clicked.connect(self.select_path)
+        
+    def select_path(self) :
+        path = QFileDialog.getExistingDirectory()
+        self.le_download_path.setText(path)
 
-        self.mainloop()
-
+    def download(self) :
+        # File Type
+        if self.rb_mp3.isChecked() :
+            filetype = 'mp3'
+        else :
+            filetype = 'mp4'
+        
+        # Quality
+        if self.rb_high.isChecked() :
+            quality = 'high'
+        elif self.rb_1080.isChecked() :
+            quality = '1080p'
+        else :
+            quality = '720p'
+        
+        # Download Path
+        path = self.le_download_path.text()
+        
+        if self.tabWidget.currentIndex() == 0 :
+            urls = []
+            for le in self.le_links :
+                if le.text() :
+                    urls.append(le.text())
+            
+            download_yts(urls, path, filetype, quality)
+            
+        elif self.tabWidget.currentIndex() == 1 :
+            url = self.le_link11.text()
+            download_yt_playlist(url, path)
+        
+        # print(f"It's {filetype} and {quality}")
+        
 if __name__ == '__main__' :
-    dyt = DownloadYouTube()
+    app = QApplication(sys.argv)
+    window = main()
+    window.show()
+    app.exec_()
