5989f7849d232acee54735d0ee47ffadb611854e
YCLin win11 <a50206123@gmail.com>
2025-09-26 00:24:31 +0800

    Add some note in code

--- yt_dlp_download.py
+++ yt_dlp_download.py
@@ -1,10 +1,16 @@
 import yt_dlp
 
+# Input : url
 url = "https://www.youtube.com/watch?v=lCX8L2bFT5I"
 
-ydl_opts = {'outtmpl': './Download MP4/%(title)s.%(ext)s',
-            'merge_output_format': 'mp4',
-            'format': 'best'}
+ydl_opts = {'outtmpl': './Download MP4/%(title)s.%(ext)s', # Download Path
+            'merge_output_format': 'mp4', # File Type
+            'format': 'bestvideo+bestaudio', # Quality
+            
+            'postprocessors': [{ # Convert MP4 to MOV using FFMPEG
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mov',
+            }]}
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url])
