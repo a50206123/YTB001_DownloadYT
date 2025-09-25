68035349f97f33e93ffea5a5209f85cf4ce8b284
YCLin win11 <a50206123@gmail.com>
2025-09-26 00:03:46 +0800

    123

--- download_yt.py
+++ download_yt.py
@@ -1,9 +1,9 @@
-from pytube import YouTube, Playlist
+from pytube import YouTube 
 
-def download_yt(url = 'https://www.youtube.com/watch?v=OnThkOMc658', folder = './', filetype = 'mp4') :
-    yt =YouTube(url, use_oauth=True, allow_oauth_cache=True)
+def download_yt(url = 'https://www.youtube.com/watch?v=OnThkOMc658', folder = 'Download MP4', filetype = 'mp4') :
+    yt =YouTube(url, on_progress_callback= on_progress)
 
-    yt.streams.filter(file_extension = filetype).get_highest_resolution().download(folder)
+    yt.streams.filter().get_highest_resolution().download(folder)
 
     print('Finished !!')
 
@@ -13,30 +13,5 @@ def on_progress(stream, chunk, remains) :
 
     print(f'Downloading ... {percent : .2f}%', end = '\r')
 
-def download_yts(urls, folder, filetype, quality):
-    num_url = len(urls)
-    
-    for url in urls :
-        yt = YouTube(url)
-        
-        if filetype == 'mp3' :
-            stream = yt.streams.filter(only_audio=True).first()
-            stream.download(folder, filename = f'{yt.title}.{filetype}')
-            print(f'{yt.title} Finished !!')
-        else :
-            if quality == 'high' :
-                stream = yt.streams.filter().get_highest_resolution()
-            elif quality == '1080p' :
-                stream = yt.streams.filter().get_by_resolution('1080p')
-            else :
-                steam = yt.streams.filter().get_by_resolution('720p')
-                
-            stream.download(folder, filename = f'{yt.title}.{filetype}')
-            
-def download_yt_playlist(url, folder = './') :
-    yt = Playlist(url)
-    
-    yt.download_all('./')
-
 if __name__ == '__main__' :
-    download_yt(folder = './', filetype = 'mp4')
+    download_yt(url = "https://www.youtube.com/watch?v=lCX8L2bFT5I")
